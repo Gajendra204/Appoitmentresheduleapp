b0e57925ba42ce338f6b83e4a90c633b6f61bfe2
Gajendra Sharma <gajendrasharma0145@gmail.com>
2025-07-06 09:22:05 +0530

    ui fixes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout() {
               },
             }}
           >
-            <Stack.Screen name="(tabs)" options={{ headerShown: true }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen
               name="appointment/[id]"
               options={{
